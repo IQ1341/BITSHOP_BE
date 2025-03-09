9460db8c2fbd019e48c86b8991bb341f7bde4af7
Muhammad Iqbal <muhammadiqb4l03@gmail.com>
2025-03-09 15:31:06 +0700

    update controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,40 @@
 import UserModel from "../models/user.model";
 
- 
+ export async function registerUserController(request,response) {
+    try {
+        const {name,email,password} = request.body
+        if (!name || !email || !password){
+            return response.status(400).json({
+                message : "Provide name,email,password",
+                error : true,
+                success : false
+            })
+        }
+        const user = await UserModel.findOne({email})
+        if(user){
+            return response.json({
+                message : "Alredy register email",
+                error : true,
+                success : false
+            })
+        }
+        const salt = await bcryptjs.genSalt(10)
+        const hashPassword = await bcryptjs.hash(password,salt)
+
+        const payload ={
+            name,
+            email,
+            password : hashPassword
+        }
+
+        const newUser = new UserModel(payload)
+        const save = await newUser.save()
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+ }
