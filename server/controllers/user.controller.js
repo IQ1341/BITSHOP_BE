f63c0f5a6afdb436697a5907ef5159bcdb00f78b
Muhammad Iqbal <muhammadiqb4l03@gmail.com>
2025-03-12 15:43:31 +0700

    forgot password

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,7 +5,9 @@ import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 import generatedAccessToken from "../utils/generatedAccessToken.js";
 import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 import uploadImageCloudinary from "../utils/uploadImageCloudinary.js";
-
+import generatedOtp from "../utils/generatedOtp.js";
+import forgotPasswordTemplate from "../utils/forgotPasswordTempplate.js";
+import jwt from "jsonwebtoken";
 
  export async function registerUserController(request,response) {
     try {
@@ -204,6 +206,233 @@ import uploadImageCloudinary from "../utils/uploadImageCloudinary.js";
         })
     } catch (error) {
         return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+ }
+
+ export async function updateUserDetails(req,res) {
+    try {
+        const userId = req.userId
+        const {name, email, mobile,password} = req.body
+
+        let hashPassword = ""
+
+        if (password) {
+            const salt = await bcryptjs.genSalt(10)
+            hashPassword = await bcryptjs.hash(password,salt)
+        }
+        const updateUser = await UserModel.updateOne({_id : userId},{
+            ...(name && {name : name}),
+            ...(email && {email : email}),
+            ...(mobile && {mobile : mobile}),
+            ...(password && {password : password})
+        })
+
+        return res.json({
+            message : "User berhasil di Update",
+            error : false,
+            success : true,
+            data : updateUser
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true ,
+            success : false
+        })
+    }
+ }
+
+ export async function forgotPasswordController(req,res) {
+    try {
+        const { email } = req.body
+
+        const user = await UserModel.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({
+                message : "Email tidak Valid",
+                error : true,
+                success : false
+            })
+        }
+
+        const otp = generatedOtp()
+        const expireTime = new Date() + 60 * 60 * 100
+
+        const update = await UserModel.findByIdAndUpdate(user._id,{
+            forgot_password_otp : otp,
+            forgot_password_expiry : new Date(expireTime).toDateString()
+        })
+
+        await sendEmail({
+            sendTo : email,
+            subject : "Forgot password from BITSHOP",
+            html : forgotPasswordTemplate ({
+                name : user.name,
+                otp : otp
+            })
+        })
+
+        return res.json({
+            message : "Cek Email anda",
+            error : false,
+            success : true,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+ }
+
+ export async function verifyForgotPasswordOtp(req,res) {
+    try {
+        const {email, otp } = req.body
+
+        if (!email || !otp) {
+            return res.status(400).json({
+                message : "Isikan email atau otp yang benar",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message : "Email not available",
+                error : true,
+                success : false
+            })
+        }
+
+        const currentTime = new Date().toDateString()
+        if (user.forgot_password_expiry < currentTime) {
+            return res.status(400).json({
+                message : "OTP is Expired",
+                error : true,
+                success : false
+            })
+        }
+
+        if (otp !== user.forgot_password_otp) {
+            return res.status(400).json({
+                message : "Invalid OTP",
+                error : true,
+                success : false
+            })
+        }
+
+        return res.json({
+            message : "Verify OTP Succesfully",
+            error : false,
+            success :true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+ }
+
+ export async function resetPassword(req,res) {
+    try {
+        const { email, newPassword, confirmPassword } = req.body
+
+        if (!email || !newPassword || !confirmPassword) {
+            return res.status(400).json({
+                message : "Provide required fields email, newPassword, confirmPassword",
+                error : true,
+                success : false
+            })
+        }
+        const user = await UserModel.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message : " Email not available",
+                error : true,
+                success : false
+            })
+        }
+        if (newPassword !== confirmPassword) {
+            return res.status(400).json({
+                message : "newPassword & confirmPassword not same",
+                error : true,
+                success : false
+            })
+        }
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashPassword = await bcryptjs.hash(newPassword,salt)
+        const update = await UserModel.findByIdAndUpdate(user._id,{
+            password : hashPassword
+        })
+
+        return res.json({
+            message : "Password update successfully",
+            error : false,
+            success : true
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+ }
+
+ export async function refreshToken(req,res) {
+    try {
+        const refreshToken = req.cookies.refreshToken || req?.header?.authorization?.split("")[1]
+
+        if (!refreshToken) {
+            return res.status(400).json({
+                message : "Unauthorized access",
+                error : true,
+                success :false
+            })
+        }
+
+        const verifyToken = await jwt.verify(refreshToken,process.env.SECRET_KEY_REFRESH_TOKEN)
+
+        if (!verifyToken) {
+            return res.status(401).json({
+                message : "token is expired",
+                error : true,
+                success : false
+            })
+        }
+
+        const userId = verifyToken?._id
+        const newAccessToken = await generatedAccessToken(userId)
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        res.cookie('accessToken',newAccessToken,cookiesOption)
+
+        return res.json({
+            message : "New access token successfully",
+            error : false,
+            success : true,
+            data : {
+                accessToken : newAccessToken
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
             message : error.message || error,
             error : true,
             success : false
